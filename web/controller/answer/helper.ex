527d397c585589dfec82ff0097688625f2985557
Wakana.Tani <wakana.tani@Y55-66750.local>
2019-05-20 10:26:19 +0900

    index

--- web/controller/answer/helper.ex
+++ web/controller/answer/helper.ex
@@ -1,22 +1,20 @@
 use Croma
 
 defmodule StackoverflowCloneA.Controller.Answer.Helper do
-  defmodule Params do
-    defmodule Body do
-      use Croma.SubtypeOfString, pattern: ~r/\A.{1,100}\z/u
+    use StackoverflowCloneA.Controller.Application
+
+    @collection_name "Answer"
+
+    defun collection_name() :: String.t do
+      @collection_name
     end
-    defmodule Question_id do
-      use Croma.SubtypeOfString, pattern: ~r/\A.{1,50}\z/u
+  
+    def to_response_body(doc) do
+      base_map = %{
+        "id"         => doc["_id"],
+        "created_at" => doc["created_at"],
+      }
+      Map.merge(doc["data"], base_map)
     end
   end
-
-  @collection_name "Answer"
-
-  defun collection_name() :: String.t do
-    @collection_name
-  end
-
-  defun to_response_body(map :: map) :: map do
-    Map.fetch!(map, "data") |> Map.put("id", map["_id"])
-  end
-end
+  
