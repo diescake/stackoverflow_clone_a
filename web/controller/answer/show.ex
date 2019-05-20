3b4b5a1ba2ea6fb5d0c313c1361ca71eb91d0c5e
Wakana.Tani <wakana.tani@Y55-66750.local>
2019-05-20 13:39:20 +0900

    answer/show.ex

--- web/controller/answer/show.ex
+++ web/controller/answer/show.ex
@@ -1,7 +1,17 @@
+
 defmodule StackoverflowCloneA.Controller.Answer.Show do
   use StackoverflowCloneA.Controller.Application
+  alias StackoverflowCloneA.Dodai, as: SD
+  alias StackoverflowCloneA.Controller.Answer.Helper
+  alias StackoverflowCloneA.Error.ResourceNotFoundError
 
-  def show(_conn) do
-    # Implement me
+  def show(%Conn{request: %Request{path_matches: %{id: id}}} = conn) do
+    req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(), "Answer", id, SD.root_key())
+
+    res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
+    case res do
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: body} -> Conn.json(conn, 200, Helper.to_response_body(body))
+      %Dodai.ResourceNotFound{}                             -> ErrorJson.json_by_error(conn, ResourceNotFoundError.new())
+    end
   end
 end
