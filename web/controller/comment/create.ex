3c21508e62f412cbc6569627fbd6cca905c00519
Wakana.Tani <wakana.tani@Y55-66750.local>
2019-05-22 10:53:46 +0900

    Answerにも対応

--- web/controller/comment/create.ex
+++ web/controller/comment/create.ex
@@ -13,34 +13,40 @@ defmodule StackoverflowCloneA.Controller.Comment.Create do
   # クライアントからのcurlのauthorizationを元にuserを取得する
   plug StackoverflowCloneA.Plug.FetchMe, :fetch, []
 
-  def create(%Antikythera.Conn{assigns: %{me: me}, request: %Antikythera.Request{body: %{"body" => body}, path_matches: %{document_id: id}}} = conn) do
+  def create(%Antikythera.Conn{assigns: %{me: me}, request: %Antikythera.Request{body: %{"body" => body}, path_info: path_info, path_matches: %{document_id: id}}} = conn) do
     # IO.inspect conn
+
+    coll = String.capitalize(Enum.at(path_info, 1))
 
     # Questionの情報をDodaiからAntikytheraがもらう
       # Dodaiからもらうために必要なものを準備
       # Dodai.RetrieveDedicatedDataEntityRequest.new("group_id", "Collection_name", q_id, "rootKey")
-      q_req = Dodai.RetrieveDedicatedDataEntityRequest.new("g_HtEsAbX7", "Question", id, "rkey_0ywy9jSuXktTvzF")
+    q_req = Dodai.RetrieveDedicatedDataEntityRequest.new("g_HtEsAbX7", coll, id, "rkey_0ywy9jSuXktTvzF")
       # 実行
-    _q_res = Sazabi.G2gClient.send(conn.context, "a_BvqzN73e", q_req)
+    q_res = Sazabi.G2gClient.send(conn.context, "a_BvqzN73e", q_req)
     # IO.inspect q_res
-    
-    random = RandomString.stream(:alphanumeric) |> Enum.take(20) |> List.to_string
-    IO.puts "--------------------"
-    IO.inspect random
-    IO.puts "--------------------"
 
-    comments = %{
+
+    # IO.inspect q_res.body["data"]["comments"]
+
+    q_res_comment = q_res.body["data"]["comments"]
+
+    random = RandomString.stream(:alphanumeric) |> Enum.take(20) |> List.to_string
+    # IO.inspect random
+    
+
+    comments = [%{
         "id"         => random,        # 上記の方法でランダムな文字列を生成
         "user_id"    => me["_id"],         # login userの`_id`
         "body"       => body,              # userが指定した値
         "created_at" => me["createdAt"],   # comment作成時の時刻 
-    }
+    }]
     # IO.inspect data
 
     # 作成したいCommentの内容の確定
-    req_body = %Dodai.CreateDedicatedDataEntityRequestBody{data: %{"$set" => %{"comments" => comments}}}
+    req_body = %Dodai.CreateDedicatedDataEntityRequestBody{data: %{"$set" => %{"comments" => q_res_comment ++ comments}}}
     # dodaiにリクエストするためには何が必要？それの準備
-    req = Dodai.UpdateDedicatedDataEntityRequest.new(SD.default_group_id(), Helper.collection_name(), id, SD.root_key(), req_body)
+    req = Dodai.UpdateDedicatedDataEntityRequest.new(SD.default_group_id(), coll, id, SD.root_key(), req_body)
     # 実行
     res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
     case res do
